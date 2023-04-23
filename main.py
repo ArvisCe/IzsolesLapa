b28b73bae4d28541d98a55778fdc15660ab461c8
ArvisCE <ArvisCE@gmail.com>
2023-04-24 01:51:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from routes.blueprintExample import blueprintExample
+from routes.authentication import auth
+from routes.home import home
+
+app = Flask(__name__)
+
+app.register_blueprint(blueprintExample, url_prefix="/bp")
+
+# authentication routes >> 
+app.register_blueprint(auth, url_prefix="")
+app.register_blueprint(home, url_prefix="")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ routes/authentication.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
+
+@auth.route("/login")
+def login():
+    return render_template("authentication/login.html")
+
+@auth.route("/register")
+def register():
+    return render_template("authentication/register.html")
