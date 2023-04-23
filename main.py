e68cad91896c27ba8c73316db4712ffcd974289e
ArvisCE <ArvisCE@gmail.com>
2023-04-24 01:52:53 +0300

    edited readme

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from routes.home import home
 
 app = Flask(__name__)
 
+
+# example blueprint
 app.register_blueprint(blueprintExample, url_prefix="/bp")
 
 # authentication routes >> 
 app.register_blueprint(auth, url_prefix="")
+
+# item view and main routes >> 
 app.register_blueprint(home, url_prefix="")
 
 
