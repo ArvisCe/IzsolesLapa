82228245ec16a277a468bcfcbb4c53e0a289286f
ArvisCE <arvisCE@gmail.com>
2023-04-24 16:08:12 +0300

    Feat: created forms in login, register, started creating database

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template
-from routes.blueprintExample import blueprintExample
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from routes.authentication import auth
 from routes.home import home
 
 app = Flask(__name__)
+app.secret_key = 'DevelopmentKey123'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///IzsoleLV.db"
+
+db = SQLAlchemy()
+db.init_app(app)
+
+# create  databases
+
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String, unique=True, nullable=False)
+  password = db.Column(db.String, unique=False, nullable=False)
+  bio = db.Column(db.String, unique=False, nullable=True)
+  gender = db.Column(db.String, unique=False, nullable=True)
+  profileImage = db.Column(db.String, unique=False, nullable=True)
+  role = db.Column(db.String, unique=False, nullable=True)
+  createdOn = db.Column(db.String, unique=False, nullable=False)
 
 
-# example blueprint
-app.register_blueprint(blueprintExample, url_prefix="/bp")
+with app.app_context():
+  db.create_all()
 
-# authentication routes >> 
+
+# authentication routes
 app.register_blueprint(auth, url_prefix="")
 
-# item view and main routes >> 
+# item view and main routes 
 app.register_blueprint(home, url_prefix="")
 
 
--- routes/authentication.py
+++ routes/authentication.py
@@ -1,11 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash, url_for
 
 auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("authentication/login.html")
+    if request.method == "GET":   
+        return render_template("authentication/login.html")
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(render_template("authentication/login.html"))
+        
 
-@auth.route("/register")
+@auth.route("/register", methods=['GET','POST'])
 def register():
-    return render_template("authentication/register.html")
+    if request.method == "GET":   
+        return render_template("authentication/register.html")
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        rPassword = request.form['repeatPassword']
+        if rPassword !=  password:
+            flash('Paroles nesakrīt!', 'error')
+            return redirect(url_for("auth.register"))
+        elif len(password) < 8 or len(password) > 64:
+            flash('Parolei jābūt starp 8 un 64 simboliem garai', 'error')
+            return redirect(url_for("auth.register"))
+        else:
+            flash('Veiksmīga reģistrācija!', 'success')
+            return redirect(url_for("home.index"))
