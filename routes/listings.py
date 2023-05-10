dc9778163cda443d583ec21f394670aa177d0cf9
ArvisCE <arvisCE@gmail.com>
2023-05-10 20:11:09 +0300

    date time huina

--- routes/listings.py
+++ routes/listings.py
@@ -52,15 +52,15 @@ def new():
     latvia_timezone = pytz.timezone('Europe/Riga')
     current_time = datetime.now(latvia_timezone)
 
-    
-    if not isinstance(auctionTime, datetime):
-        errors += 1
-        flash("Tu nedrīksti vienkārši rakstīt ko tu gribi datetime. ip grabbed.",'error')
-    else:
+    try:
         auctionTime = datetime.strptime(request.form['auctionTime'], '%Y-%m-%dT%H:%M')
         if auctionTime.replace(tzinfo=None) - current_time.replace(tzinfo=None) < timedelta(days=1):
             flash('Izsole nedrīkst notikt ātrāk par 24 stundām nākotnē!','error')
             errors += 1
+    except ValueError:
+        errors += 1
+        flash("Tu nedrīksti vienkārši rakstīt ko tu gribi datetime. ip grabbed.",'error')
+        
     if errors > 0:
         return redirect(url_for('listing.new'))
     new_listing = Listing(
